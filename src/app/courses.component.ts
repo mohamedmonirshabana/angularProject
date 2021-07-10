9585d66e91e51af9b4ed781982531664d21ba8b7
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-10 22:49:09 +0200

    Start angullar App

---
+++ src/app/courses.component.ts
@@ -0,0 +1,10 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'courses', //   <div class="courses"> ".courses" / <courses> "courses" / <div id="courses"> "#Courses"
+    template: '<h2>Courses</h2>'
+})
+export class CoursesComponent{
+
+}

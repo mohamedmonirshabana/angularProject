fc0c1bc99b3a7b3d04f3304c568cbf0bcde3ffba
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-11 00:42:48 +0200

    Change Template in Courses Component

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', //   <div class="courses"> ".courses" / <courses> "courses" / <div id="courses"> "#Courses"
-    template: '<h2>Courses</h2>'
+    template: '<h2>{{ "Title:" + title }}</h2>'
 })
 export class CoursesComponent{
+    title = "List of Courses";
 
+    getTitle() {
+        return this.title;
+    }
 }

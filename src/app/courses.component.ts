1cf175a85c00edd747d1de8b42eaf68d5ae35cd2
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-11 00:45:51 +0200

    Add GetTitle() Method in Atemplate

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', //   <div class="courses"> ".courses" / <courses> "courses" / <div id="courses"> "#Courses"
-    template: '<h2>{{ "Title:" + title }}</h2>'
+    template: '<h2>{{ getTitle() }}</h2>'
 })
 export class CoursesComponent{
     title = "List of Courses";

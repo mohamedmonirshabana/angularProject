9c5ef8cba50789a361b6afd9f49c74910278bc96
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-11 17:07:18 +0200

    add attribute bunding to app

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,15 +4,16 @@ import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses', //   <div class="courses"> ".courses" / <courses> "courses" / <div id="courses"> "#Courses"
-    template: `
-        <h2>{{ title }}</h2>
-        <h2 [textContent]="title"></h2>
-        
-        <img src="{{ imageUrl }}" />
-        <img [src]="title" />
+    template: `     
+        <img [src]="imageUrl" />
+        <table>
+            <tr>
+                <td [attr.colspan]="colSpan></td>
+            </tr>
+        </table>
         `
 })
 export class CoursesComponent{
-    title = "List of Courses";
-    imageUrl = "http://lorempixel.com/400/200";
+    imageUrl = "";
+    colSpan = 2;
 }

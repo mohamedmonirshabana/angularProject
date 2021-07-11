603504a78a326b346a24d1bcb7c5a998375802a2
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-11 17:35:23 +0200

    add style binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,10 +4,11 @@ import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses', //   <div class="courses"> ".courses" / <courses> "courses" / <div id="courses"> "#Courses"
-    template: `     
-            <button class="btn btn-primary" [class.active]="isActive">Save</button>
+    template: `         
+            <button [style.backgroundColor="isActive ? 'blue': 'white'"]>Save</button>
         `
+    //search for dom style object properties
 })
 export class CoursesComponent{
-    isActive = false;  //true or false
+    isActive = true;  //true or false
 }

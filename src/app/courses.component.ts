3f889e0117bb7be6135c6f7b0c5116e9d5f3dfde
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-12 01:28:42 +0200

    Add event binding for input text and Fix Issue in button

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,18 +5,19 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses', //   <div class="courses"> ".courses" / <courses> "courses" / <div id="courses"> "#Courses"
     template: `
-            <div (click)="onDiveClicked()">         
-            <button (click)="onSave()">Save</button>
-            </div>
+            <input (keyup)="onKeyUp($event)" />
+
+            <button (click)="onSave($event)">save</button>
         `
     //search for dom style object properties
 })
 export class CoursesComponent{
-    onDiveClicked() {
-        console.log("Dive wase click");
+    onKeyUp(event:any) {
+        if (event.keyCode === 13) console.log("ENTER was Pressed");
     }
-    onSave() {
-        
-        console.log("button wase click");
+
+    onSave(event: any) {
+        event.p
+        console.log("button was Enter",event);
     }
 }

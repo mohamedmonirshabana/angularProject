ba134a3223c32aeedc6263635901f6e0933eb386
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-12 13:22:48 +0200

    pipe Work test in pipe

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,7 +5,7 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses', //   <div class="courses"> ".courses" / <courses> "courses" / <div id="courses"> "#Courses"
     template: `
-            {{ course.title | uppercase }} <br/>
+            {{ course.title | uppercase | lowercase}} <br/>
             {{ course.student | number }} <br/>
             {{ course.rating | number:'1.2-2' }} <br/>
             {{ course.price | currency: 'AUD':true:'3.2-2' }} <br/>
@@ -16,7 +16,7 @@ import { CoursesService } from './courses.service';
 export class CoursesComponent{
     course = {
         title: "The Complete Angular Course",
-        student: 200,
+        student: 30123,
         rating: 4.9745,
         price: 190.95,
         releaseDate: new Date(2016, 3, 1)

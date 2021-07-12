9960e61534b768fc0fbe36a74d83b351b61ab342
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-12 13:40:12 +0200

    Add custome pipe file and implement in component

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,20 +5,12 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses', //   <div class="courses"> ".courses" / <courses> "courses" / <div id="courses"> "#Courses"
     template: `
-            {{ course.title | uppercase | lowercase}} <br/>
-            {{ course.student | number }} <br/>
-            {{ course.rating | number:'1.2-2' }} <br/>
-            {{ course.price | currency: 'AUD':true:'3.2-2' }} <br/>
-            {{ course.releaseDate | date:'shortDate'}} <br/>
+            {{ text | summary }}
         `
     
 })//search for dom style object properties
 export class CoursesComponent{
-    course = {
-        title: "The Complete Angular Course",
-        student: 30123,
-        rating: 4.9745,
-        price: 190.95,
-        releaseDate: new Date(2016, 3, 1)
-    }
+    text = `
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+    `
 }

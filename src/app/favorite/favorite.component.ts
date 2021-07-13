31198a3d14ce8f163220d3d0bf44a2b60b65f35e
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-13 02:00:00 +0200

    finish add style file and style embaded

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -4,16 +4,25 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
     selector: 'favorite',
     templateUrl: './favorite.component.html',
+    
+    styles: [
+        `
+             .glyphicon{
+                color:green;
+            }
+            .glyphicon-star{
+                background:black;
+            }
+        `
+    ],
     styleUrls: ['./favorite.component.css'],
+     
     // inputs: ['isFavorite']
 })
-export class FavoriteComponent implements OnInit{
+export class FavoriteComponent{
     @Input('isFavorite') isSelected!: boolean;
     @Output('change') click = new EventEmitter();
-
-    constructor() { }
     
-    ngOnInit() { }
 
     onClick() {
         this.isSelected = !this.isSelected;

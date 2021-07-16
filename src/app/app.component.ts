f0b08b26ea8cf88f69d91c11e5851df6e072ba44
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-16 15:31:25 +0200

    Add ngif directive to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,5 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  post = {
-    title: "Title",
-    isFavorite: true
-  }
-  onFavoriteChange(eventArgs: FavoriteChangedEventArgs) {
-    console.log("favorite Change", eventArgs);
-  }
+  courses = [1, 2];
 }

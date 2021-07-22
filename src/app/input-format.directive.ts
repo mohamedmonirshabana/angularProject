ab562d81646f39308fd29294b02d9f5730fdb9af
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-22 03:42:40 +0200

    add Directive format for upper and lower

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,17 +1,18 @@
-import { Directive, HostListener, ElementRef } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
-  // @HostListener('focus')onFocus() {
-  //   console.log("on Focus")
-  // }
+  @Input('format') format:any;
   constructor(private el:ElementRef) { }
 
   @HostListener('blur')onBlur() {
     let value: string = this.el.nativeElement.value;
-    this.el.nativeElement.value = value.toLowerCase();
+    if (this.format == 'lowercase')
+      this.el.nativeElement.value = value.toLowerCase();
+    else
+      this.el.nativeElement.value = value.toUpperCase();
   }
 
 }

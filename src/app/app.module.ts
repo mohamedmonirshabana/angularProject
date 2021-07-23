05b1c90fbece11cddcc6fcfe5a6d6c45e1e0de0d
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-23 15:08:33 +0200

    add contact form for Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { PanelComponent } from './panel/panel.component';
 import { MtestComponent } from './mtest/mtest.component';
 import { InputFormatDirective } from './input-format.directive';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { InputFormatDirective } from './input-format.directive';
     PanelComponent,
     MtestComponent,
     InputFormatDirective,
+    ContactFormComponent,
   ],
   imports: [
     BrowserModule,

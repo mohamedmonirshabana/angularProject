0c15c387418d71a445a1c06af63ca9780c7cd1d4
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-26 18:47:28 +0200

    add Form by Code without Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,6 +32,7 @@ import { SignuoFormComponent } from './signuo-form/signuo-form.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     CoursesService,
--- src/app/signuo-form/signuo-form.component.ts
+++ src/app/signuo-form/signuo-form.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'signuo-form',
   templateUrl: './signuo-form.component.html',
   styleUrls: ['./signuo-form.component.css']
 })
-export class SignuoFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SignuoFormComponent {
+  form = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl()
+  });
 }

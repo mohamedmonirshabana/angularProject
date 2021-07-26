d40e1c5fe431dd7ad8c0e71d2c9de1fa48d475bf
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-26 20:58:21 +0200

    form by code is work now update it

--- src/app/signuo-form/signuo-form.component.ts
+++ src/app/signuo-form/signuo-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'signuo-form',
@@ -8,7 +8,11 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class SignuoFormComponent {
   form = new FormGroup({
-    username: new FormControl(),
-    password: new FormControl()
+    username: new FormControl('',Validators.required),
+    password: new FormControl('',Validators.required)
   });
+
+  get username() {
+    return this.form.get('username');
+  }
 }

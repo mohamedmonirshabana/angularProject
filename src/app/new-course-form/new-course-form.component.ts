dd18d79d44bf6bb17069d3c3fef688b3bf93ad10
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-28 14:50:20 +0200

    Add form Array With error

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-course-form',
   templateUrl: './new-course-form.component.html',
   styleUrls: ['./new-course-form.component.css']
 })
-export class NewCourseFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class NewCourseFormComponent  {
+  form = new FormGroup({
+    topics: new FormArray([])
+  });
+  addTopic(topic: HTMLInputElement) {
+    (this.form.get('topics') as FormArray).push(new FormControl(topic.value));
+    topic.value = '';
   }
-
 }

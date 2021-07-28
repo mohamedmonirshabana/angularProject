bc90759dc2a7672f6c7128b1d8651192174668e0
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-29 01:44:21 +0200

    update Data from jsonplaceHolder

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -30,4 +30,11 @@ export class PostsComponent {
         console.log(response);
        });
   }
+  updatePost(post: any) {
+    this.http.patch(this.url+'/'+post.id, JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response);
+      });
+    // this.http.put(this.url, JSON.stringify(post));
+  }
 }
